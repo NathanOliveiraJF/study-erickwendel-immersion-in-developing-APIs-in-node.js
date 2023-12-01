284915aa66e5c8614d89b04d1b6092db26d134df
Nathan Oliveira <nathanoliveira003jf@gmail.com>
2023-12-01 08:52:22 -0300

    wip

--- modulo-4/06-cli/database.js
+++ modulo-4/06-cli/database.js
@@ -1,6 +1,7 @@
-const { readFile, readFileSync } = require('fs')
+const { readFile, writeFile} = require('fs')
 const { promisify } = require('util')
 const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
 class Database {
   constructor() {
     this.NOME_ARQUIVO = 'herois.json'
@@ -9,8 +10,23 @@ class Database {
     const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
     return JSON.parse(arquivo.toString())
   }
-  escreverArquivo() {
-
+  async escreverArquivo(dados) {
+    await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados))
+    return true
+  }
+  async cadastrar(heroi) {
+    const dados = await this.obterDadosArquivo()
+    const id = heroi.id <= 2 ? heroi.id : Date.now()
+    const heroiComId = {
+      id, 
+      ...heroi,
+    }
+    const dadosFinal = [
+      ...dados,
+      heroiComId
+    ]
+    const resultado = await this.escreverArquivo(dadosFinal)
+    return resultado
   }
   async listar(id) {
     const dados = await this.obterDadosArquivo()
